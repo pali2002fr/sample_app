929149a65e5506ea4366df248d50a0f315153b74
palitchatokey <pali2002fr@yahoo.fr>
2013-09-24 14:39:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '9373ae3ef648dcd75dea02b139a1469a3f5fc10a5c28a3ef3ac0d172328a49c4fe22e2ba3a42c138c64b61a74a472d419bdbfc7969694e3d6ffc4c61e7e61e69'
